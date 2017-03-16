9848cf17f811b9080c6eb593b4e65cc3e5523d04
yuri.belozertsev <yuri.belozertsev@nakitel.com>
2017-03-16 15:51:11 +0200

    My first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function () {
+    var accItem = document.getElementsByClassName('accordion-item');
+    var accHD = document.getElementsByClassName('accordion-item-title');
+    for (i = 0; i < accHD.length; i++) {
+        accHD[i].addEventListener('click', toggleItem, false);
+    }
+    function toggleItem() {
+        var itemClass = this.parentNode.className;
+        for (i = 0; i < accItem.length; i++) {
+            accItem[i].className = 'accordion-item close';
+        }
+        if (itemClass == 'accordion-item close') {
+            this.parentNode.className = 'accordion-item open';
+        }
+    }
+})();
